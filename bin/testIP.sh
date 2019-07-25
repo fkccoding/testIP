704ef1c5a76efea42eea4c84c4dfc900f1cdc44d
fkccoding <35401547+fkccoding@users.noreply.github.com>
2019-07-25 21:19:23 +0800

    Add Mac version
    适配Mac端的脚本

--- bin/testIP.sh
+++ bin/testIP.sh
@@ -4,13 +4,13 @@ ip1=${ip0%.*};ip2=${ip0##*.}
 # echo ip1=$ip1 ip2=$ip2
 read -p "请输入要测试的连续IP地址个数：" num
 num=`expr $ip2 + $num`
-echo =====================
+echo ===============================
 for ((ip2;ip2<num;ip2++))
 do
-ping=`ping -c 1 $ip1.$ip2|grep loss|awk '{print $(NF-4)}'|awk -F "%" '{print $1}'`
+ping=`ping -c 1 $ip1.$ip2|grep loss|awk '{print $(NF-2)}'|awk -F "." '{print $1}'`
 if [ $ping -eq 100 ]; then
-  echo -e $ip1.$ip2 is "\E[1;31mDOWN\E[0m"
+  echo -e $ip1.$ip2 is "DOWN"
 else
-  echo -e $ip1.$ip2 is "\E[1;32mUP\E[0m"
+  echo -e $ip1.$ip2 is "UP"
 fi
 done
